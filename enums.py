b25e2428989c2983cd708f5b099b7e5de75f49a6
Esa Varemo <esa@kuivanto.fi>
2012-08-05 23:33:59 +0300

    File structure + enums

---
+++ enums.py
@@ -0,0 +1,23 @@
+#Port name enums:
+GPIO_0 = 0
+GPIO_1 = 1
+GPIO_4 = 4
+GPIO_7 = 7
+GPIO_8 = 8
+GPIO_9 = 9
+GPIO_10 = 10
+GPIO_11 = 11
+GPIO_14 = 14
+GPIO_15 = 15
+GPIO_17 = 17
+GPIO_18 = 18
+GPIO_21 = 21
+GPIO_22 = 22
+GPIO_23 = 23
+GPIO_24 = 24
+GPIO_25 = 25
+
+
+#Port Direction enums:
+IN  = 0
+OUT = 1
